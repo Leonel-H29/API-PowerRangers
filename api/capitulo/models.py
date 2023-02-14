1555161730a9e225247f2036acb3a6e9d6bf403b
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-14 11:02:14 -0300

    Agrego la clase de Capitulo

--- api/capitulo/models.py
+++ api/capitulo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE, DO_NOTHING
+from django.db.models.fields import AutoField
+from temporada.models import temporada
 
 # Create your models here.
+class capitulo(models.Model):
+    id_capitulo = AutoField(primary_key=True)
+    nombre=models.CharField(max_length=20)
+    descripcion=models.CharField(max_length=150)
+    id_temporada = models.ForeignKey(temporada, on_delete=DO_NOTHING)
