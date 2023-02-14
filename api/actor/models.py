15ebcb7ecd2af46aaf04e1cd32f005d646579912
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-14 11:19:07 -0300

    Agrego la clase de 'Aparecen en'

--- api/actor/models.py
+++ api/actor/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.deletion import CASCADE, DO_NOTHING
 from django.db.models.fields import AutoField
+from temporada.models import temporada
 
 # Create your models here.
 class actor(models.Model):
@@ -17,3 +18,10 @@ class personaje(models.Model):
     descripcion=models.CharField(max_length=150)
     foto=models.CharField(max_length=270)
     id_actor= models.ForeignKey(actor, on_delete=DO_NOTHING)
+    #temporada=models.ManyToManyField(temporada, related_name='aparecen')
+    
+class aparecen(models.Model):
+    id_aparicion = AutoField(primary_key=True)
+    id_personaje= models.ForeignKey(personaje, on_delete=DO_NOTHING)
+    id_temporada = models.ForeignKey(temporada, on_delete=DO_NOTHING)
+    rol=models.CharField(max_length=10)
