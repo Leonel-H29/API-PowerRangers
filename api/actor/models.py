4a42fb8fe61fbedaf6735179bcda4cc02eb53961
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-14 10:52:52 -0300

    Agrego la clase de Personaje

--- api/actor/models.py
+++ api/actor/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
+from django.db.models.deletion import CASCADE, DO_NOTHING
 from django.db.models.fields import AutoField
 
 # Create your models here.
@@ -9,3 +9,11 @@ class actor(models.Model):
     nombre_artistico=models.CharField(max_length=80, blank=True, null=True)
     foto=models.CharField(max_length=270)
     biografia=models.CharField(max_length=270)
+
+
+class personaje(models.Model):
+    id_personaje= AutoField(primary_key=True)
+    nombre_personaje=models.CharField(max_length=80)
+    descripcion=models.CharField(max_length=150)
+    foto=models.CharField(max_length=270)
+    id_actor= models.ForeignKey(actor, on_delete=DO_NOTHING)
