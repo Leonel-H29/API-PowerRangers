e80662a427cb60f5fdf9af3510bcbbd19838e55b
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-21 20:08:50 -0300

    Actualizo el ejecutable

--- api/git_add_all.sh
+++ api/git_add_all.sh
@@ -5,6 +5,8 @@ git add .
 echo -n "- Ingrese un mensaje para el nuevo commit: "
 read message
 
-git commit -m $message
+git commit -m "$message"
 
 git push origin $(git branch --show-current)
+
+git log
