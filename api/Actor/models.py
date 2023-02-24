052f57305df96260c30036d0f9db23684198b316
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-24 20:57:22 -0300

    Modifico el modelo de personajes y aparencen

--- api/Actor/models.py
+++ api/Actor/models.py
@@ -26,11 +26,27 @@ class personaje(models.Model):
     nombre_personaje=models.CharField(max_length=80)
     descripcion=models.TextField(blank=True, null=True)
     foto=models.CharField(max_length=260)
-    id_actor= models.ForeignKey(actor, on_delete=DO_NOTHING)
+    id_actor= models.ForeignKey(actor, on_delete=DO_NOTHING, db_column='id_actor')
     #temporada=models.ManyToManyField(temporada, related_name='aparecen')
     
+    def __str__(self):
+        return str(self.id_personaje) + ")- " + self.nombre + "-- " + str(self.id_actor) 
+    
+    class Meta:
+        db_table='personajes'
+        verbose_name='personaje'
+        verbose_name_plural='personajes'
+        ordering = ['nombre_personaje']
+
+
 class aparecen(models.Model):
     id_aparicion = AutoField(primary_key=True)
-    id_personaje= models.ForeignKey(personaje, on_delete=CASCADE)
-    id_temporada = models.ForeignKey(temporada, on_delete=CASCADE)
+    id_personaje= models.ForeignKey(personaje, on_delete=CASCADE, db_column='id_personaje')
+    id_temporada = models.ForeignKey(temporada, on_delete=CASCADE, db_column='id_temporada')
     rol=models.CharField(max_length=10)
+    
+    class Meta:
+        db_table='aparecen'
+        verbose_name='aparece'
+        verbose_name_plural='aparecen'
+        ordering = ['id_aparicion']
