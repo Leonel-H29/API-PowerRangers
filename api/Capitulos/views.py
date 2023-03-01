c7fabd0fca0b8cad24d89fd06b5cb60a0499b0ea
Leonel-H29 <leonelherreradkno@gmail.com>
2023-02-28 21:40:06 -0300

    Agrego filtros en el API de Capitulos

--- api/Capitulos/views.py
+++ api/Capitulos/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.mixins import PermissionRequiredMixin
 #from rest_framework.decorators import action
 #from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
 from Capitulos.models import capitulo
 from Capitulos.serializers import CapitulosSerializer
@@ -12,3 +13,5 @@ from Capitulos.serializers import CapitulosSerializer
 class CapitulosViewSet(viewsets.ModelViewSet):
     queryset = capitulo.objects.all()
     serializer_class = CapitulosSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['numero_cap', 'temporada']
