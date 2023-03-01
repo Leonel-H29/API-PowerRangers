9f358a2461c1b9106f10c5d423c67fb1cb573c3c
Leonel-H29 <leonelherreradkno@gmail.com>
2023-03-01 11:43:20 -0300

    Agrego filtros en la API de Actores

--- api/Actor/views.py
+++ api/Actor/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.mixins import PermissionRequiredMixin
 #from rest_framework.decorators import action
 #from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 
 from Actor.models import actor, aparecen, personaje
 from Actor.serializers import ActorSerializer, AparecenEnSerializer, PersonajeSerializer
@@ -12,6 +13,8 @@ from Actor.serializers import ActorSerializer, AparecenEnSerializer, PersonajeSe
 class ActoresViewSet(viewsets.ModelViewSet):
     queryset = actor.objects.all()
     serializer_class = ActorSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['nombre_actor', 'nombre_artistico']
     name = 'actores'
     depth = 1
     
