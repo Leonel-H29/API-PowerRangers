eae44d8afb864f539655396b8e9fac1856a82aa1
Leonel-H29 <leonelherreradkno@gmail.com>
2023-03-31 17:54:02 -0300

    Update del_migrate.sh

--- api/del_migrate.sh
+++ api/del_migrate.sh
@@ -3,14 +3,14 @@
 #Elimino la base de datos
 rm db.sqlite3
 
-"""
-Busco los directorios con el nombre de '__pycache__' y 'migrations'
-"""
+
+##Busco los directorios con el nombre de '__pycache__' y 'migrations'
+
 directorios=$(find . -type d -name "__pycache__" && find . -type d -name "migrations")
 
-"""
-Recorro la lista y elimino cada directorio y el contenido de cada directorio
-"""
+
+#Recorro la lista y elimino cada directorio y el contenido de cada directorio
+
 for x in $directorios; do
 	#echo $x
 	rm -r $x
