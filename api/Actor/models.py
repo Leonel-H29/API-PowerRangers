be7eefadb578390858ad59571f0ed6922683d910
Leonel-H29 <leonelherreradkno@gmail.com>
2023-04-01 20:47:47 -0300

    Modifico la longitud de algunos campos

--- api/Actor/models.py
+++ api/Actor/models.py
@@ -6,10 +6,10 @@ from Temporadas.models import temporada
 # Create your models here.
 class actor(models.Model):
     id_actor= AutoField(primary_key=True)
-    nombre_actor=models.CharField(max_length=80, unique=True)
+    nombre_actor=models.CharField(max_length=80)
     nombre_artistico=models.CharField(max_length=80, blank=True, null=True)
-    foto=models.CharField(max_length=260)
-    biografia=models.CharField(max_length=260)
+    foto=models.CharField(max_length=300)
+    biografia=models.CharField(max_length=300)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     
@@ -27,7 +27,7 @@ class personaje(models.Model):
     id_personaje= AutoField(primary_key=True)
     nombre_personaje=models.CharField(max_length=80, unique=True)
     #descripcion=models.TextField(blank=True, null=True)
-    foto=models.CharField(max_length=260)
+    foto=models.CharField(max_length=300)
     #id_actor= models.ForeignKey(actor, on_delete=DO_NOTHING, db_column='id_actor')
     actor= models.ForeignKey(actor, on_delete=DO_NOTHING, db_column='id_actor',related_name='personajes')
     temporadas=models.ManyToManyField(temporada,through='Aparecen', related_name='personajes')
@@ -49,7 +49,7 @@ class aparecen(models.Model):
     id_aparicion = AutoField(primary_key=True)
     personaje= models.ForeignKey(personaje, on_delete=CASCADE, db_column='id_personaje')
     temporada = models.ForeignKey(temporada, on_delete=CASCADE, db_column='id_temporada')
-    rol=models.CharField(max_length=10)
+    rol=models.CharField(max_length=30)
     descripcion=models.TextField(blank=True, null=True)
     class Meta:
         db_table='aparecen'
