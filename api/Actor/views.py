c40396c21468139e42bf41a1753f0502e23036fd
Leonel-H29 <leonelherreradkno@gmail.com>
2023-04-03 12:18:14 -0300

    Agrego detalles a cada funcion de actores para su documentacion en Swagger

--- api/Actor/views.py
+++ api/Actor/views.py
@@ -11,7 +11,8 @@ from Actor.serializers import ActorSerializer, AparecenEnSerializer, PersonajeSe
 from rest_framework import permissions 
 from User.permissions import SuperuserPermission, ReadOnlyPermission
 
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class ActoresViewSet(viewsets.ModelViewSet):
@@ -22,7 +23,93 @@ class ActoresViewSet(viewsets.ModelViewSet):
     name = 'actores'
     depth = 1
     authentication_classes = [TokenAuthentication]
-    permission_classes = [SuperuserPermission | ReadOnlyPermission]    
+    permission_classes = [SuperuserPermission | ReadOnlyPermission]
+    
+    @swagger_auto_schema(
+        operation_summary="Obtener un actor por su id",
+        operation_description="Retorna un actor con la información completa.",
+        responses={200: ActorSerializer()})
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Obtener todos los actores",
+        operation_description="Retorna una lista con todos los actores.",
+        responses={200: ActorSerializer(many=True)})
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Crear un nuevo actor",
+        operation_description="Crea un nuevo actor con la información proporcionada.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'nombre_actor': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre real del actor'),
+                'nombre_artistico': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre artístico del actor'),
+                'foto': openapi.Schema(type=openapi.TYPE_STRING, description='URL de la foto del actor'),
+                'biografia': openapi.Schema(type=openapi.TYPE_STRING, description='URL de la biografia del actor'),
+            }
+        ),
+        responses={
+            201: ActorSerializer(),
+            400: 'Error en los datos enviados',
+            401: 'No autenticado',
+            403: 'Permiso denegado'
+        },
+        security=[{'Token de acceso': []}]
+        )     
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Actualizar un actor existente",
+        operation_description="Actualiza la información de un actor existente.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'nombre_actor': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre real del actor'),
+                'nombre_artistico': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre artístico del actor'),
+                'edad': openapi.Schema(type=openapi.TYPE_INTEGER, description='Edad del actor'),
+                'pais': openapi.Schema(type=openapi.TYPE_STRING, description='País del actor'),
+            }
+        ),
+        responses={200: ActorSerializer()})
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        request_body=ActorSerializer,
+        operation_summary="Actualiza parcialmente un actor",
+        operation_description='Actualiza parcialmente un actor existente',
+        responses={
+            200: 'Actor actualizado exitosamente',
+            400: 'Error en los datos enviados',
+            401: 'No autenticado',
+            403: 'Permiso denegado',
+            404: 'No encontrado'
+        },
+        security=[{'Token de acceso': []}]
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Elimina un actor",
+        operation_description='Elimina un actor existente',
+        responses={
+            204: 'Actor eliminado exitosamente',
+            401: 'No autenticado',
+            403: 'Permiso denegado',
+            404: 'No encontrado'
+        },
+        security=[{'Token de acceso': []}]
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+    
+
+
     
 class AparecenViewSet(viewsets.ModelViewSet):
     queryset = aparecen.objects.all()
@@ -31,6 +118,8 @@ class AparecenViewSet(viewsets.ModelViewSet):
     depth = 1
     authentication_classes = [TokenAuthentication]
     permission_classes = [SuperuserPermission | ReadOnlyPermission]    
+
+
 
 class PersonajesViewSet(viewsets.ModelViewSet):
     queryset = personaje.objects.all()
