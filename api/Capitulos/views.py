7c74c7573af3ab2c5aa54e15a1b76e9b8994a892
Leonel Herrera <48606307+Leonel-H29@users.noreply.github.com>
2023-04-04 15:55:41 -0300

    Actualizo la documentacion para Capitulos

--- api/Capitulos/views.py
+++ api/Capitulos/views.py
@@ -21,3 +21,86 @@ class CapitulosViewSet(viewsets.ModelViewSet):
     #filterset_fields = ['numero_cap', 'temporada']
     filterset_fields = ['numero_cap']
     permission_classes = [SuperuserPermission | ReadOnlyPermission]
+
+    @swagger_auto_schema(
+        operation_summary="Obtener un capitulo por su id",
+        operation_description="Retorna un capitulo con la información completa.",
+        responses={200: CapitulosSerializer()})
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Obtener todos los capitulos",
+        operation_description="Retorna una lista con todos los capitulos.",
+        responses={200: CapitulosSerializer(many=True)})
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Crear un nueva capitulo",
+        operation_description="Crea un nueva capitulo con la información proporcionada.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'numero_cap' : openapi.Schema(type=openapi.TYPE_INTEGER, description='Numero del capitulo'),
+                'nombre': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre real del capitulo'),
+                'descripcion': openapi.Schema(type=openapi.TYPE_STRING, description='Descripcion de la temporada'),
+                'temporada': openapi.Schema(type=openapi.TYPE_ARRAY, description='Temporada a la que pertenece el capitulo'),              
+            }
+        ),
+        responses={
+            201: CapitulosSerializer(),
+            400: 'Error en los datos enviados',
+            401: 'No autenticado',
+            403: 'Permiso denegado'
+        },
+        security=[{'Token de acceso': []}]
+        )     
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Actualizar un actor existente",
+        operation_description="Actualiza la información de un actor existente.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'numero_cap' : openapi.Schema(type=openapi.TYPE_INTEGER, description='Numero del capitulo'),
+                'nombre': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre real del capitulo'),
+                'descripcion': openapi.Schema(type=openapi.TYPE_STRING, description='Descripcion de la temporada'),
+                'temporada': openapi.Schema(type=openapi.TYPE_ARRAY, description='Temporada a la que pertenece el capitulo'),              
+            }
+        ),
+        responses={200: CapitulosSerializer()})
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        request_body=CapitulosSerializer,
+        operation_summary="Actualiza parcialmente un capitulo",
+        operation_description='Actualiza parcialmente un capitulo existente',
+        responses={
+            200: 'Actor actualizado exitosamente',
+            400: 'Error en los datos enviados',
+            401: 'No autenticado',
+            403: 'Permiso denegado',
+            404: 'No encontrado'
+        },
+        security=[{'Token de acceso': []}]
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Elimina un capitulo",
+        operation_description='Elimina un capitulo existente',
+        responses={
+            204: 'Actor eliminado exitosamente',
+            401: 'No autenticado',
+            403: 'Permiso denegado',
+            404: 'No encontrado'
+        },
+        security=[{'Token de acceso': []}]
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
