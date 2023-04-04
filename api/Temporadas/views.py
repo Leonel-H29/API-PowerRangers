9c15c47765811654d4de5ef5f58446a4d5463536
Leonel Herrera <48606307+Leonel-H29@users.noreply.github.com>
2023-04-04 15:44:28 -0300

    Actualizo detalles de la documentacion para Temporadas

--- api/Temporadas/views.py
+++ api/Temporadas/views.py
@@ -20,3 +20,94 @@ class TemporadasViewSet(viewsets.ModelViewSet):
     depth = 1
     authentication_classes = [TokenAuthentication]
     permission_classes = [SuperuserPermission | ReadOnlyPermission]
+
+    @swagger_auto_schema(
+        operation_summary="Obtener un temporada por su id",
+        operation_description="Retorna un temporada con la información completa.",
+        responses={200: TemporadaSerializer()})
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Obtener todas las temporadaes",
+        operation_description="Retorna una lista con todas las temporadaes.",
+        responses={200: TemporadaSerializer(many=True)})
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Crear una nueva temporada",
+        operation_description="Crea un nueva temporada con la información proporcionada.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'numero_temporada' : openapi.Schema(type=openapi.TYPE_INTEGER, description='Numero de temporada'),
+                'nombre' : openapi.Schema(type=openapi.TYPE_STRING, description='Nombre de esa temporada'),
+                'descripcion': openapi.Schema(type=openapi.TYPE_STRING, description='Descripcion de la temporada'),
+                'foto' : openapi.Schema(type=openapi.TYPE_STRING, description='URL de la foto de la temporada'),
+                'anio_estreno': openapi.Schema(type=openapi.TYPE_INTEGER, description='Año de estreno de la temporada'),
+                'cancion' : openapi.Schema(type=openapi.TYPE_STRING, description='Nombre de la cancion de esa temporada'),
+                'basada_en' : openapi.Schema(type=openapi.TYPE_STRING, description='Nombre del serie, novela, libro, etc en que se basa esa temporada'),
+                'tematica' : openapi.Schema(type=openapi.TYPE_STRING, description='La tematica en que se basa esa temporada'),
+            }
+        ),
+        responses={
+            201: TemporadaSerializer(),
+            400: 'Error en los datos enviados',
+            401: 'No autenticado',
+            403: 'Permiso denegado'
+        },
+        security=[{'Token de acceso': []}]
+        )     
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        operation_summary="Actualizar un temporada existente",
+        operation_description="Actualiza la información de un temporada existente.",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'numero_temporada' : openapi.Schema(type=openapi.TYPE_INTEGER, description='Numero de temporada'),
+                'nombre' : openapi.Schema(type=openapi.TYPE_STRING, description='Nombre de esa temporada'),
+                'descripcion': openapi.Schema(type=openapi.TYPE_STRING, description='Descripcion de la temporada'),
+                'foto' : openapi.Schema(type=openapi.TYPE_STRING, description='URL de la foto de la temporada'),
+                'anio_estreno': openapi.Schema(type=openapi.TYPE_INTEGER, description='Año de estreno de la temporada'),
+                'cancion' : openapi.Schema(type=openapi.TYPE_STRING, description='Nombre de la cancion de esa temporada'),
+                'basada_en' : openapi.Schema(type=openapi.TYPE_STRING, description='Nombre del serie, novela, libro, etc en que se basa esa temporada'),
+                'tematica' : openapi.Schema(type=openapi.TYPE_STRING, description='La tematica en que se basa esa temporada'),
+            }
+        ),
+        responses={200: TemporadaSerializer()})
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        request_body=TemporadaSerializer,
+        operation_summary="Actualiza parcialmente una temporada",
+        operation_description='Actualiza parcialmente una temporada existente',
+        responses={
+            200: 'Temporada actualizada exitosamente',
+            400: 'Error en los datos enviados',
+            401: 'No autenticado',
+            403: 'Permiso denegado',
+            404: 'No encontrado'
+        },
+        security=[{'Token de acceso': []}]
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    @swagger_auto_schema(
+        operation_summary="Elimina una temporada",
+        operation_description='Elimina una temporada existente',
+        responses={
+            204: 'Temporada eliminada exitosamente',
+            401: 'No autenticado',
+            403: 'Permiso denegado',
+            404: 'No encontrado'
+        },
+        security=[{'Token de acceso': []}]
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
