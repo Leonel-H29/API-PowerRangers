3462cec396914b990fa160bace636834879177d9
Leonel-H29 <leonelherreradkno@gmail.com>
2023-04-06 20:05:55 -0300

    Agrego detalles a la documentacion de Login en Swagger

--- api/User/views.py
+++ api/User/views.py
@@ -11,13 +11,52 @@ from User.serializers import UserLoginSerializer, UserModelSerializer
 # Models
 from User.models import User
 
+#API SWAGGER
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 class UserViewSet(viewsets.GenericViewSet):
 
     queryset = User.objects.filter(is_active=True)
     serializer_class = UserModelSerializer
     lookup_field = 'username'
 
-
+    @swagger_auto_schema(
+        operation_summary="Inicio de sesion de un usuario",
+        operation_description="Permite a un usuario iniciar sesión en el sistema mediante su correo electrónico y contraseña.",
+        manual_parameters=[
+            openapi.Parameter(
+                name='Email',
+                in_=openapi.IN_QUERY,
+                required=True,
+                type=openapi.TYPE_STRING,
+                description='Ingrese su correo electronico'
+            ),
+            openapi.Parameter(
+                name='Password',
+                in_=openapi.IN_QUERY,
+                required=True,
+                type=openapi.TYPE_STRING,
+                description='Ingrese su contraseña'
+            ),
+        ],
+        
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'email': openapi.Schema(type=openapi.TYPE_STRING, description='Correo del usuario'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Contraseña del usuario'), 
+                #'nombre_actor': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre real del actor'),
+                #'nombre_artistico': openapi.Schema(type=openapi.TYPE_STRING, description='Nombre artístico del actor'),
+                #'edad': openapi.Schema(type=openapi.TYPE_INTEGER, description='Edad del actor'),
+                #'pais': openapi.Schema(type=openapi.TYPE_STRING, description='País del actor'),
+            }
+        ),
+        responses={
+            #200: 'Inicio de sesion exitoso',
+            201: 'Inicio de sesion exitoso',
+            400: 'Usuario o contraseña no validos',
+    })
     @action(detail=False, methods=['post'])
     def login(self, request):
         """User sign in."""
@@ -33,7 +72,10 @@ class UserViewSet(viewsets.GenericViewSet):
         else:
             return Response({'error': 'Usuario o contraseña no validos'}, status=status.HTTP_400_BAD_REQUEST)
         
-        return Response({'message': 'Inicio de sesion'}, status=status.HTTP_200_OK)
+        #return Response({'message': 'Inicio de sesion'}, status=status.HTTP_200_OK)
+    
+        
+        
 
     """
     @action(detail=False, methods=['post'])
@@ -44,3 +86,5 @@ class UserViewSet(viewsets.GenericViewSet):
         data = UserModelSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)
     """
+    
+    
