c7eae178284578db681f9372c4caa7ef2aaf721d
Leonel-H29 <leonelherreradkno@gmail.com>
2023-04-15 10:28:08 -0300

    Agrego el model del User al panel de administracion de Django

--- api/User/admin.py
+++ api/User/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=('username', 'email', 'is_superuser', 'is_staff' ,'is_active')
+    search_fields=('username', 'email')
+    ordering=['username']
+    
+admin.site.register(User, UserAdmin)
