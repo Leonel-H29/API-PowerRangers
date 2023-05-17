814f03ed4fa4e34b940480ae3e623ae8cefe3c4e
Leonel-H29 <leonelherreradkno@gmail.com>
2023-05-17 15:18:29 -0300

    Agrego configuraciones a los archivos para crear los contenedores

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -6,11 +6,12 @@ dirs_app=$(ls -d */)
 delimiter='/'
 
 command="manage.py makemigrations"
+config="--settings=api.settings.development"
 
 for x in $dirs_app; do
 	#Separo la cadena
 	app=$(echo $x | cut -d$delimiter -f1)
-	
+
 	#Verifico si el nombre de la cadena no es igual al del directorio principal
 	#y al directorio para hacer scraping de datos
 	if [ "$app" != "api" ] && [ "$app" != "data" ]; then
@@ -20,20 +21,19 @@ for x in $dirs_app; do
 done
 
 #Creo las migraciones para crear la base de datos
-echo "Realizo makemigrations"
+echo "Realizo makemigrations ..."
 python3 $command
 sleep 2
 
 #Creamos las tablas
-echo "Realizo migrate"
-python3 manage.py migrate 
+echo "Realizo migrate ..."
+python3 manage.py migrate $config
 sleep 2
 
 #Inicio el servidor
-echo "Inicio el servidor"
-python3 manage.py runserver 0.0.0.0:8081
+echo "Inicio el servidor ..."
+python3 manage.py runserver 0.0.0.0:8081 $config
 
 echo "El backend esta listo"
 
 exec "$@"
-
