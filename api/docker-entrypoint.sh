1b686d4cf38b45d587ec74976630e2756fc7c578
Leonel-H29 <leonelherreradkno@gmail.com>
2023-05-21 15:10:24 -0300

    Realizo modificaciones en los .sh para la ejecucion del proyecto tanto de forma local como para produccion

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -4,17 +4,27 @@
 dirs_app=$(ls -d */)
 #Delimitador de cadena
 delimiter='/'
+#Array de directorios que no son apps
+no_apps=("api" "data" "venv" "database")
 
 command="manage.py makemigrations"
-config="--settings=api.settings.development"
+config="--settings=api.settings.production"
 
 for x in $dirs_app; do
 	#Separo la cadena
 	app=$(echo $x | cut -d$delimiter -f1)
 
-	#Verifico si el nombre de la cadena no es igual al del directorio principal
-	#y al directorio para hacer scraping de datos
-	if [ "$app" != "api" ] && [ "$app" != "data" ]; then
+	value=0
+	
+	#Verifico si el nombre de la cadena pertenece al array de no_apps
+	for dir in "${no_apps[@]}"; do
+		if [ "$app" == "$dir" ]; then
+			value=1
+			break
+		fi
+	done
+
+	if [ $value == 0 ]; then
 		#Concateno para formar el resto del comando
 		command="$command $app"
 	fi
@@ -32,7 +42,7 @@ sleep 2
 
 #Inicio el servidor
 echo "Inicio el servidor ..."
-python3 manage.py runserver 0.0.0.0:8081 $config
+python3 manage.py runserver 0.0.0.0:8000 $config
 
 echo "El backend esta listo"
 
--- api/make_migrations.sh
+++ api/make_migrations.sh
@@ -4,22 +4,48 @@
 dirs_app=$(ls -d */)
 #Delimitador de cadena
 delimiter='/'
+#Array de directorios que no son apps
+no_apps=("api" "data" "venv" "database")
 
 command="manage.py makemigrations"
+config="--settings=api.settings.development"
 
 for x in $dirs_app; do
 	#Separo la cadena
 	app=$(echo $x | cut -d$delimiter -f1)
+
+	value=0
 	
-	#Verifico si el nombre de la cadena no es igual al del directorio principal
-	#y al directorio para hacer scraping de datos
-	if [ "$app" != "api" ] && [ "$app" != "data" ]; then
+	#Verifico si el nombre de la cadena pertenece al array de no_apps
+	for dir in "${no_apps[@]}"; do
+		if [ "$app" == "$dir" ]; then
+			value=1
+			break
+		fi
+	done
+
+	if [ $value == 0 ]; then
 		#Concateno para formar el resto del comando
 		command="$command $app"
 	fi
 done
 
+#echo $command
+
 #Creo las migraciones para crear la base de datos
+echo "Realizo makemigrations ..."
 python3 $command
+sleep 2
+
 #Creamos las tablas
-python3 manage.py migrate 
+echo "Realizo migrate ..."
+python3 manage.py migrate $config
+sleep 2
+
+#Inicio el servidor
+echo "Inicio el servidor ..."
+python3 manage.py runserver $config
+
+echo "El backend esta listo"
+
+exec "$@"
