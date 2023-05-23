4dce285cbf4330a45ba0ea6ad2abc35ae728adaf
Leonel-H29 <48606307+Leonel-H29@users.noreply.github.com>
2023-05-23 09:28:36 -0300

    Comento linea en apps.py de User y realizo un control de las variables de entorno en production.py

--- api/api/settings/production.py
+++ api/api/settings/production.py
@@ -10,13 +10,31 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "DATABASE_PORT": config("DB_PORT"),
-    },
+#Configuracion de la base de datos Postgres
+DB_NAME=config("DB_NAME")
+DB_USER=config("DB_USER")
+DB_PASSWORD=config("DB_PASSWORD")
+DB_HOST=config("DB_HOST")
+DB_PORT=config("DB_PORT")
+
+POSTGRES_READY = (
+    DB_NAME is not None
+    and DB_USER is not None
+    and DB_PASSWORD is not None
+    and DB_HOST is not None
+    and DB_PORT is not None
+)
+
+print(POSTGRES_READY)
+
+if POSTGRES_READY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": config("DB_NAME"),
+            "USER": config("DB_USER"),
+            "PASSWORD": config("DB_PASSWORD"),
+            "HOST": config("DB_HOST"),
+            "DATABASE_PORT": config("DB_PORT"),
+        },
 }
