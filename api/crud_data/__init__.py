f57a3b4a9e3b352891192ff97adb0f74b59ab0e4
Leonel-H29 <leonelherreradkno@gmail.com>
2023-05-28 16:29:45 -0300

    Modifico la clase de personajes

--- api/crud_data/__init__.py
+++ api/crud_data/__init__.py
@@ -5,6 +5,7 @@ from db_settings import DBSettings
 from crud_actores import CrudActores
 from crud_temporadas import CrudTemporadas
 from crud_capitulos import CrudCapitulos
+from crud_personajes import CrudPersonajes
 from time import sleep
 
 
@@ -14,6 +15,7 @@ if __name__ == "__main__":
     Actor = CrudActores()
     Temp = CrudTemporadas()
     Cap = CrudCapitulos()
+    Pers = CrudPersonajes()
 
     if DB.conect_db():
         # print(DB.conect_db())
@@ -22,18 +24,20 @@ if __name__ == "__main__":
         # print(DB.len_table_db('actor'))
 
         # Clases independientes
-        print(Fore.RESET + "----Temporadas: ")
-        Temp.get_temporadas_file()
-        sleep(1)
-        print(Fore.RESET + "----Actores: ")
-        Actor.get_actores_file()
-        sleep(1)
+        # print(Fore.RESET + "----Temporadas: ")
+        # Temp.get_temporadas_file()
+        # sleep(1)
+        # print(Fore.RESET + "----Actores: ")
+        # Actor.get_actores_file()
+        # sleep(1)
 
         # Clases dependientes
         print(Fore.RESET + "----Capitulos: ")
         Cap.get_capitulos_file()
         sleep(1)
-        # print(Fore.RESET + "----Personajes: ")
+        print(Fore.RESET + "----Personajes: ")
+        Pers.get_personajes_file()
+        sleep(1)
         # GetPersonajes()
         # print(Fore.RESET + "----Aparecen: ")
         # GetAparecen()
--- api/crud_data/crud_personajes.py
+++ api/crud_data/crud_personajes.py
@@ -1,80 +1,124 @@
 import xlrd
 import os
 from dotenv import load_dotenv
-from conect_db import conect_db
+from db_settings import DBSettings
 from colorama import Fore
 from datetime import datetime
 import psycopg2
 
+load_dotenv()
+
+
 class CrudPersonajes():
-    load_dotenv()
-    conn = conect_db()
-    conn.autocommit = True
-    
-    def get_personajes() : 
-        file = os.getenv('FILE_DATA')
-        openFile = xlrd.open_workbook(file)
-        #Indico con que hoja voy a trabajar
+    def __init__(self) -> None:
+        # pass
+        self.DB = DBSettings()  # Inicio una instancia de 'DBSettings'
+        self.conn = self.DB.conect_db()
+        self.conn.autocommit = True
+        self.file = os.getenv('FILE_DATA')  # Archivo donde extraigo los datos
+        self.db_table_name = 'personajes'
+        self.db_table_name_fk = 'actor'
+
+    # Funcion para retornar una lista con datos unicos
+
+    def uniq_data(self, dic: dict = {}, list_data: list = []) -> list:
+        try:
+            pers = dic["nombre_personaje"]
+            idactor = dic["id_actor"]
+
+            # Verifico si el personaje esta cargado en la base de datos
+            if self.personaje_exist(pers=pers, actor=idactor):
+                return list_data
+
+            if len(list_data) > 0:
+                for data in list_data:
+                    if pers == data["nombre_personaje"] and idactor == data["id_actor"]:
+                        return list_data
+
+                return list_data.append(dic)
+
+            # Verifica si hay datos en la tabla de la DB
+            if self.DB.len_table_db(self.db_table_name) == 0:
+                return list_data.append(dic)
+
+            # En caso de que no se cumplan ningunas de las condiciones retorno la lista
+            return list_data
+        except Exception:
+            print(Fore.RED + "Dictionary not will be null")
+            return list_data
+
+    # Funcion para extraer los datos del archivo
+    def get_personajes_file(self):
+        # Abro el archivo
+        openFile = xlrd.open_workbook(self.file)
+        # Indico con que hoja voy a trabajar
         sheet = openFile.sheet_by_name("Personajes")
 
         list_insert = []
 
         for i in range(1, sheet.nrows):
-            col1 = sheet.cell_value(i,0) #Nombre del Personaje
-            col2 = int(sheet.cell_value(i,1)) #Numero de temporada
-            col3 = sheet.cell_value(i,2) #Rol
-            col4 = sheet.cell_value(i,3) #Descripcion
-            col5 = sheet.cell_value(i,4) #Foto 
-            col6 = sheet.cell_value(i,5) #Nombre del actor
-            fyh=datetime.now()
-            
-        subquery = "SELECT id_actor FROM actor WHERE nombre_artistico='{0}' LIMIT 1".format(col6)
-        exist = existPersonaje(col1,subquery)
-        if exist==True:
-            #print(getIdPersonaje(col1,subquery))
-            #pass
-            set = "nombre_personaje='{0}', foto='{1}', updated='{2}', id_actor=({3})".format(col1,col5,fyh,subquery)
-            put_personajes(getIdPersonaje(col1,subquery),set)
-        else:   
-            values = "('{0}','{1}','{2}','{3}',({4})),".format(col1,col5,fyh,fyh,subquery)
-            #print(values)
-            list_insert.append(values)
-    
-        if len(list_insert) > 0:
-            #Busco el ultimo elemeto de la lista
-            index = len(list_insert) - 1
-            
-            #Le quito el ',' al ultimo registro y luego lo reemplazo por ';'
-            list_insert[index] = list_insert[index][0:len(list_insert[index])-1]
-            list_insert[index] += ';'
-    
-            post_personajes(list_insert)
-        else:
-            print(Fore.RED + "Lista vacia para insertar datos")
-    
+            col1 = sheet.cell_value(i, 0)  # Nombre del Personaje
+            # col2 = int(sheet.cell_value(i, 1))  # Numero de temporada
+            # col3 = sheet.cell_value(i, 2)  # Rol
+            # col4 = sheet.cell_value(i, 3)  # Descripcion
+            col5 = sheet.cell_value(i, 4)  # Foto
+            col6 = sheet.cell_value(i, 5)  # Nombre del actor
+
+        query = "SELECT id_actor FROM {0} WHERE nombre_artistico='{1}';".format(
+            self.db_table_name, col6)
+
+        actor = self.DB.get_id(query=query)
+        if actor > 0:
+            dic = {
+                "nombre_personaje": col1,
+                "foto": col5,
+                "created": datetime.now(),
+                "updated": datetime.now(),
+                "id_actor": actor
+            }
+
+            # Verifico si el actor ya se encuentra registrado
+            list_insert = self.uniq_data(dic, list_insert)
+        self.prepare_query_insert()
+
     def put_personajes(personajes):
-        cursor = conn.cursor()
-        #print(data)
-        query = "UPDATE personajes SET {0} WHERE id_personaje={1};".format(data,id)  
-        #print(query)
-        try:
-            #pass
-            cursor.execute(query)
-            print(Fore.GREEN + "Datos actualizados")
-        except:
-            print(Fore.RED + 'Error al actualizar los datos')
-        cursor.close()
-    
-    def post_personajes(n, data):
-        cant: int = len(personajes)
-        cursor = conn.cursor()
-        query = "INSERT INTO personajes (nombre_personaje, foto, created, updated, id_actor) VALUES "  
-        try:
-            for i in range(0, cant):
-                query += personajes[i]
-            cursor.execute(query)
-            print(Fore.GREEN + "Datos insertados")   
-        except psycopg2.Error as e:
-            #raise Exception('Error al insertar los datos')
-            print(Fore.RED + f'Error al insertar los datos - {e}')
-        cursor.close()
+        pass
+
+    def prepare_query_insert(self, personajes: list = []) -> None:
+
+        list_values = []
+
+        list_values = [
+            "('{0}','{1}','{2}','{3}',{4}),".format(
+                cap["nombre_personaje"],
+                cap["foto"],
+                cap["created"],
+                cap["updated"],
+                cap["id_actor"]
+            )for cap in sorted(personajes, key=lambda x: (x["nombre_personaje"], x["id_actor"]))
+        ]
+
+        if len(list_values) > 0:
+            # Ordeno la lista
+            query = ",".join(list_values)
+            query += ";"
+
+            # print(list_values)
+            self.post_capitulos(capitulos=query)
+        else:
+            print(Fore.YELLOW + "Lista vacia para insertar datos")
+
+    # Funcion para hacer un insert en la DB
+
+    def post_personajes(self, personajes: str = None) -> None:
+        query = "INSERT INTO personajes (nombre_personaje, foto, created, updated, id_actor) VALUES "
+        query += personajes
+
+    # Funcion para saber si el personaje existe
+
+    def personaje_exist(self, pers: str = None, actor: int = 0) -> bool:
+        subquery = "SELECT id_actor FROM {0} WHERE id_actor={1}".format(
+            self.db_table_name_fk, actor)
+        query = "SELECT * FROM {0} WHERE nombre_personaje='{1}' AND id_actor=({2})".format(
+            self.db_table_name, pers, subquery)
+        return self.DB.exists_tuple(query=query)
