00d7317255150dafb435ef3adf0865a489ceb2c7
Leonel-H29 <leonelherreradkno@gmail.com>
2023-07-01 20:59:51 -0300

    Constructor de actores hereda de su clase padre

--- api/Actor/crud_actores.py
+++ api/Actor/crud_actores.py
@@ -2,22 +2,17 @@ import xlrd
 # import os
 # from dotenv import load_dotenv
 from api.db_settings import DBSettings
+from api.crud_parent import CrudParent
 from colorama import Fore
 from datetime import datetime
 
 # load_dotenv()
 
 
-class CrudActores():
+class CrudActores(CrudParent):
 
-    def __init__(self, DBstt: DBSettings, file_data: str = None) -> None:
-        # pass
-        self.DB = DBstt  # Inicio una instancia de 'DBSettings'
-        self.conn = self.DB.conect_db()
-        self.conn.autocommit = True
-        self.file = file_data  # Archivo donde extraigo los datos
-        self.sheet_file = 'Actores'
-        self.db_table_name = 'actor'
+    def __init__(self, DBstt: DBSettings, sheet: str = None, tableName: str = None) -> None:
+        super().__init__(DBstt, sheet, tableName)
 
     # Funcion para retornar una lista con datos unicos
 
