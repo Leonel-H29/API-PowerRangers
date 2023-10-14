f42153955ca7894fea983b8d3d68d8b74171eafd
Leonel Herrera <48606307+Leonel-H29@users.noreply.github.com>
2023-10-14 12:59:48 -0300

    Modifico los atributos de publicos a  privados y agrego metodos GETTERs

--- api/api/crud_parent.py
+++ api/api/crud_parent.py
@@ -13,75 +13,82 @@ class CrudParent():
 
     def __init__(self, DBstt: DBSettings, sheet: str = None, tableName: str = None) -> None:
         # pass
-        self.DB = DBstt  # Inicio una instancia de 'DBSettings'
-        self.conn = self.DB.conect_db()
-        self.conn.autocommit = True
-        self.file = FILE_DATA  # Archivo donde extraigo los datos
-        self.sheet_file = sheet  # Hoja especifica deñ archivo
-        self.db_table_name = tableName  # Nombre de la tabla en la base de datos
+        self.__DB = DBstt  # Inicio una instancia de 'DBSettings'
+        self.__conn = self.__DB.conect_db()
+        self.__conn.autocommit = True
+        self.__file = FILE_DATA  # Archivo donde extraigo los datos
+        self.__sheet_file = sheet  # Hoja especifica del archivo
+        self.__db_table_name = tableName  # Nombre de la tabla en la base de datos
 
-        @property
-        def file(self): return self.file
+    @property
+    def file(self):
+        """
+        Funcion GETTER para el nombre del archivo de donde se extre los datos
 
-        @property
-        def db_table_name(self) -> str:
-            """
-            Funcion GETTER para el nombre de la tabla en la base de datos
+        ### Returns:
+            `str`: Nombre del fichero
+        """
+        return self.__file
 
-            ### Returns:
-                `str`: Nombre de la tabla 
-            """
-            return self.db_table_name
+    @property
+    def DB(self) -> DBSettings:
+        """
+        Funcion GETTER el objeto de la clase `DBSettings`
 
-        @db_table_name.setter
-        def db_table_name(self, table_name):
-            """
-            Funcion SETTER para el nombre de la tabla en la base de datos
+        ### Returns:
+            `DBSettings`: Objeto de la clase `DBSettings`
+        """
+        return self.__DB
 
-            ### Args:
-                `table_name (str)`: Nombre de la tabla 
-            """
-            self.db_table_name = table_name
+    @property
+    def db_table_name(self) -> str:
+        """
+        Funcion GETTER para el nombre de la tabla en la base de datos
 
-        @property
-        def sheet_file(self) -> str:
-            """
-            Funcion GETTER para el nombre de la hoja de datos
+        ### Returns:
+            `str`: Nombre de la tabla 
+        """
+        return self.__db_table_name
 
-            ### Returns:
-                `str`: Nombre de la tabla 
-            """
-            return self.sheet_file
+    @property
+    def sheet_file(self) -> str:
+        """
+        Funcion GETTER para el nombre de la hoja de datos
+
+        ### Returns:
+            `str`: Nombre de la tabla 
+        """
+        return self.__sheet_file
 
         # Funcion para realizar un insert multiple
 
-        def __prepare_query_insert(self, list_data: list = []) -> None:
-            """
-            La funcion que encarga de ordenar los registros para poder realizar la consulta en la base de datos
+    def __prepare_query_insert(self, list_data: list = []) -> None:
+        """
+        La funcion que encarga de ordenar los registros para poder realizar la consulta en la base de datos
 
-            ### Args:
-                `list_data (list)`: Lista de los valores a insertar en la base de datos
-            """
-            pass
+        ### Args:
+            `list_data (list)`: Lista de los valores a insertar en la base de datos
+        """
+        pass
 
         # Funcion para extraer los datos del archivo
-        def get_data_file(self):
-            """
-            La funcion se encarga de extraer todos los datos de la hoja
-            """
-            pass
+    def get_data_file(self):
+        """
+        La funcion se encarga de extraer todos los datos de la hoja
+        """
+        pass
 
-         # Funcion para retornar una lista con datos unicos
-        def __uniq_data(self, dic: dict = {}, list_data: list = []) -> list:
-            """
-            La funcion se encarga de controlar que los datos extraidos dentro de la lista `list_data` sean unicos, es decir, datos no repetidos dentro de
-            la lista, como asi que no esten ya cargados en la base de datos
+        # Funcion para retornar una lista con datos unicos
+    def __uniq_data(self, dic: dict = {}, list_data: list = []) -> list:
+        """
+        La funcion se encarga de controlar que los datos extraidos dentro de la lista `list_data` sean unicos, es decir, datos no repetidos dentro de
+        la lista, como asi que no esten ya cargados en la base de datos
 
-            ### Args:
-                - `dic (dict)`: Diccionario con los valores extraidos en cada fila de la hoja
-                - `list_data`: Lista de datos a cargarse en la base de datos
+        ### Args:
+            - `dic (dict)`: Diccionario con los valores extraidos en cada fila de la hoja
+            - `list_data`: Lista de datos a cargarse en la base de datos
 
-            ### Returns:
-                `list`: Lista de datos unicos
-            """
-            return list_data
+        ### Returns:
+            `list`: Lista de datos unicos
+        """
+        return list_data
